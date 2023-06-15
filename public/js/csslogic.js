b8b364a8b0c3d207eb6043694e0b8af1582d2d89
RdySetShine <126523453+RdySetShine@users.noreply.github.com>
2023-06-15 19:09:02 -0400

    finished html / css

---
+++ public/js/csslogic.js
@@ -0,0 +1,31 @@
+const startButton = document.querySelector(".cta-button")
+const signUpBtn = document
+
+// console.log(startButton);
+startButton.addEventListener('mouseenter', (event) => { // event listner means activating the button // (event) => { running the event to take place for previous action   }
+// console.log('cliking a button');
+startButton.innerHTML = '$$$  Click here Now! $$$';
+})
+
+startButton.addEventListener('mouseout', (event) => {
+
+    startButton.innerHTML = '$$$ Start Budgeting Now! $$$'
+})
+
+
+startButton.addEventListener('click', (event) => {
+window.location.href = 'expenses.html' // href is clicking a link directly to the html you created  *use this methode for linking html easiest
+
+});
+
+
+
+
+// add an event listent to the budget page when they submit a budget a window prompts to display number of saves left
+// save their number of clicks to local sotarge so it doesnt resest for futur use making the registration a must onward
+// they have 0 left ask " Would you like to Register?"
+// if they have 0 left they should not be able to proceed with any further budgeting plans
+// homepage even listner 'onload' (event => {}) so that it starts as soon as they open our browser
+// this should save and/or check for the number of budgets left
+// make a new variable for each new html with effects that activate.
+
