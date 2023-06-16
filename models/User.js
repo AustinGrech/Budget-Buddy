28450c4af492f4bd33c6ba1f42f147043eb99a6e
jaychan0125 <joycechan0125@gmail.com>
2023-06-15 20:21:07 -0400

    added functionality for signin/up and auth

--- models/User.js
+++ models/User.js
@@ -1,7 +1,12 @@
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class User extends Model {}
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 User.init(
   {
@@ -11,7 +16,7 @@ User.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    name: {
+    username: {
       type: DataTypes.STRING,
       allowNull: false,
     },
@@ -23,13 +28,18 @@ User.init(
         isEmail: true,
       },
     },
-    // YALL BUT DON'T STORE PASSWORDS 
     password: {
       type: DataTypes.STRING,
       allowNull: false,
     },
   },
   {
+    hooks: {
+      async beforeCreate(newUser) {
+          newUser.password = await bcrypt.hash(newUser.password,10);
+          return newUser;
+      }
+  },
     sequelize,
     timestamps: false,
     freezeTableName: true,
